73df47693233a13b30edbf8eb2801be86709e3e6
jaraJulieta <jarajuli13@gmail.com>
2023-06-29 20:48:49 -0300

    mas estilos

--- game-app/src/components/Game.js
+++ game-app/src/components/Game.js
@@ -1,6 +1,6 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-import './Game.css'; 
+import './Game.css';
 import options from "../data/options";
 import useChoice from "../useChoice/useChoices";
 import GameResult from "./GameResult";
@@ -12,24 +12,25 @@ const Game = () => {
 
     return (
         <div className="container-juego" >
+            <div className="title-juego"> ¡Piedra, Papel, Tijera, Lagarto o Spock! </div>
             <div className="styles">
-            <div> ¡Piedra, Papel, Tijera, Lagarto o Spock! </div>
-            <div className="button-container">
-                {options.map((option) => (
-                    <ButtonOptions
-                        key={option.id}
-                        option={option}
-                        disabled={disabled}
-                        handlePlay={handlePlay}
-                    />
-                ))}
-            </div>
+                <div className="comencemos"> ¡Comencemos! Elige una opción: </div>
+                <div className="button-container">
+                    {options.map((option) => (
+                        <ButtonOptions
+                            key={option.id}
+                            option={option}
+                            disabled={disabled}
+                            handlePlay={handlePlay}
+                        />
+                    ))}
+                </div>
 
-            {playerChoice != null && <div> {playerMessage}  </div>}
-            {result != null && <GameResult result={result} />}
-            {computerChoice != null && <div>{computerMessage}  </div>}
+                {playerChoice != null && <div> {playerMessage}  </div>}
+                {result != null && <GameResult result={result} />}
+                {computerChoice != null && <div>{computerMessage}  </div>}
 
-            <ButtonReset onClick={volverAJugar} />
+                <ButtonReset onClick={volverAJugar} />
             </div>
         </div>
     )
