b99a9a681e5678621fa48f6b5b1185e69b7e82e9
jaraJulieta <jarajuli13@gmail.com>
2023-07-12 14:53:48 -0300

    sintaxis mas bonita

--- game-app/src/components/GameResult.js
+++ game-app/src/components/GameResult.js
@@ -6,10 +6,26 @@ import './GameResult.css';
 
 const GameResult = ({ result }) => {
     const resultMessages = {
-        0: <div className="result">Empate<img src= {empate} alt="Imagen de igual" className="tamaño-igual" /></div>,
-        1: <div className="result">Has ganado esta partida!<img src= {caritaFeliz} alt="Imagen de ganador" className="tamaño-carita" /></div>,
-        2: <div className="result">Has perdido esta partida<img src= {caritaTriste} alt="Imagen de perdedor" className="tamaño-carita" /></div>
+        0: (
+            <div className="result">
+                Empate
+                <img src={empate} alt="Imagen de igual" className="tamaño-igual" />
+            </div>
+        ),
+        1: (
+            <div className="result">
+                Has ganado esta partida!
+                <img src={caritaFeliz} alt="Imagen de ganador" className="tamaño-carita" />
+            </div>
+        ),
+        2: (
+            <div className="result">
+                Has perdido esta partida
+                <img src={caritaTriste} alt="Imagen de perdedor" className="tamaño-carita" />
+            </div>
+        )
     };
+
 
     return <div>{resultMessages[result] || null}</div>;
 };
